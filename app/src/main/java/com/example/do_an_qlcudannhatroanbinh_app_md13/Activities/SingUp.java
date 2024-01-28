dd39d54caa48617509938c2f024ab534b15873bd
Admin <dangthehao1412@gmail.com>
2024-01-28 19:44:07 +0700

    haodangthe

---
+++ app/src/main/java/com/example/do_an_qlcudannhatroanbinh_app_md13/Activities/SingUp.java
@@ -0,0 +1,16 @@
+package com.example.do_an_qlcudannhatroanbinh_app_md13.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.do_an_qlcudannhatroanbinh_app_md13.R;
+
+public class SingUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+    }
+}
--- app/src/main/java/com/example/do_an_qlcudannhatroanbinh_app_md13/Adapter/SingUp.java
+++
@@ -1,4 +0,0 @@
-package com.example.do_an_qlcudannhatroanbinh_app_md13.Adapter;
-
-public class SingUp {
-}
